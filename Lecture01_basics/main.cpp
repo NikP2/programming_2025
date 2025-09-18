0df4a9ef4117f29aef0e8bfe8ea74a7552c3b51b
Nikita Popov <nikp180407@gmail.com>
2025-09-18 21:11:56 +0200

    A lot of Lines

--- Lecture01_basics/main.cpp
+++ Lecture01_basics/main.cpp
@@ -3,5 +3,8 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << 123 << std::endl;
+    std::cout << 123 << std::endl;
+    std::cout << 123 << std::endl;
+    std::cout << 123 << std::endl;
     return 0;
 }
