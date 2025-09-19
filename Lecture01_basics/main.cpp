131b702cc5906e7baca618e01af2d9458a5c501f
Nikita Popov <nikp180407@gmail.com>
2025-09-19 15:45:17 +0200

    Update file Lab1 Variant4

--- Lecture01_basics/main.cpp
+++ Lecture01_basics/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <cmath> //подключение math для использования функции pow
 
+using namespace std;
+
 int main() {
     const float num = 13.1415f; //создание float переменной
-    double V, S, r; //созданиеи числа с плавующей точкой с пощью double
-    std::cin >> r; //input
+    double V, S, r; //созданиеи числа с плавующей точкой с помощью double
+    cin >> r; //input
     V = pow(r, 3) * (4.0/3.0) * num;  //вычислить объём сферы
     S = 4 * num * pow(r, 2); //вычислить площадь поверхности сферы
-    std::cout << "Объём сферы: " << V << " double" << "\n" << "Площадь поверхности сферы: " << S << " double" << std::endl; // вывод переменных
-    std::cout <<"float "<< "32 бита, " << "minSize: -3.4E+38, maxSize: 3.4E+38" << std::endl;
-    std::cout <<"double "<< "64 бита, " << "minSize: -1.7E-308, maxSize: 1.7E-308" << std::endl;
+    cout << "Объём сферы: " << V << " double" << "\n" << "Площадь поверхности сферы: " << S << " double" << endl; // вывод переменных
+    cout <<"float "<< "32 бита, " << "minSize: -3.4E+38, maxSize: 3.4E+38" << endl;
+    cout <<"double "<< "64 бита, " << "minSize: -1.7E-308, maxSize: 1.7E-308" << endl;
     return 0;
 }
