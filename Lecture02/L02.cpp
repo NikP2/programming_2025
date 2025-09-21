67b4ace57e7ef24b3cbfc951a643e22a597e69a8
Nikita Popov <nikp180407@gmail.com>
2025-09-21 13:54:12 +0200

    ADD test file

---
+++ Lecture02/L02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char firstName[50];
+    cout << "Введите имя: ";
+    cin >> firstName;
+    cout << "Hello " << firstName << endl;
+    return 0;
+}
