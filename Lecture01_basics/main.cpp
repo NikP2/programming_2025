e57548c7784bf7554ad669794b7c9cb9c1c87c39
Nikita Popov <nikp180407@gmail.com>
2025-09-26 15:01:49 +0200

    ADD:lowestInNumeric_limits

--- Lecture01_basics/main.cpp
+++ Lecture01_basics/main.cpp
@@ -11,8 +11,8 @@ int main() {
     V = pow(r, 3) * (4.0 / 3.0) * num;  //вычислить объём сферы
     S = 4 * num * pow(r, 2); //вычислить площадь поверхности сферы
     cout << "Объём сферы: " << V << " double" << "\n" << "Площадь поверхности сферы: " << S << " double" << endl; // вывод переменных
-    cout <<"float "<< sizeof(float) * 8 << " бита, " << -numeric_limits<float>::min()  << " " <<  numeric_limits<float>::max()  << endl;
-    cout <<"double "<< sizeof(double) * 8 << " бита, " << numeric_limits<double>::min() << " " << numeric_limits<double>::max()  << endl;
+    cout <<"float "<< sizeof(float) * 8 << " бита, " << numeric_limits<float>::lowest()  << " " <<  numeric_limits<float>::max()  << endl;
+    cout <<"double "<< sizeof(double) * 8 << " бита, " << numeric_limits<double>::lowest() << " " << numeric_limits<double>::max()  << endl;
     cout <<"int "<< sizeof(int) * 8 << " бита, " << numeric_limits<int>::min() << " " << numeric_limits<int>::max() << endl;
     return 0;
 }
