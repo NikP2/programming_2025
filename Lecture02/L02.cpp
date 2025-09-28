112228490fcaa2f6f26879590bb7340e1ccda960
Nikita Popov <nikp180407@gmail.com>
2025-09-28 17:00:54 +0200

    New L2

--- Lecture02/L02.cpp
+++ Lecture02/L02.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-
-using namespace std;
+#include <limits>
+#include <bitset>
+//#define _USE_MATH_DEFINES
+//#include <math.h>
 
 int main() {
-    char firstName[50];
-    cout << "Введите имя: ";
-    cin >> firstName;
-    cout << "Hello " << firstName << endl;
-    cerr << "firstname" << firstName << endl;
-    return 0;
+   int x = 10;
+   std::bitset<8> b2 = x;
+   std::cout << b2.to_string() << std::endl;
 }
+
+/*#include <iostream>
+#include <numbers>
+  int main() {
+std::cout << std::numbers::pi << std::endl;
+}*/
+
