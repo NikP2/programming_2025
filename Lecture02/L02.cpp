f8aaf8a025d40a22457f998c87191780f44f3d33
Nikita Popov <nikp180407@gmail.com>
2025-09-30 22:44:12 +0200

    L02Done

--- Lecture02/L02.cpp
+++ Lecture02/L02.cpp
@@ -1,18 +1,81 @@
 #include <iostream>
-#include <limits>
-#include <bitset>
-//#define _USE_MATH_DEFINES
-//#include <math.h>
+using namespace std;
 
-int main() {
-   int x = 10;
-   std::bitset<8> b2 = x;
-   std::cout << b2.to_string() << std::endl;
-}
+// Number 1
 
-/*#include <iostream>
-#include <numbers>
-  int main() {
-std::cout << std::numbers::pi << std::endl;
-}*/
+/*int main() {
+  int A, B, C, i;
+  cout << "Число i: ";
+  cin >> i;
+  cout << "Число A: ";
+  cin >> A;
+  cout << "Число B: ";
+  cin >> B;
+  cout << "Число C: ";
+  cin >> C;
+  if (A & (1 << i)) {
+    if ((A < B) && (B < C)) {
+      cout << C-B-A << endl;
+    }
+    else if (A % C == 0) {
+      cout << A/C+B << endl;
+    }
+    else {
+      cout << A+B+C << endl;
+    }
+  }
+  else {
+    cout << (A << 1) << endl;
+  }
+  return 0;
+} */
 
+// Number 2
+
+/*int main() {
+  int n;
+  cout << "Введите номер месяца: ";
+  cin >> n;
+  switch (n)
+  {
+  case 1:
+    cout << "January: 31 days" << endl;
+    break;
+  case 2:
+    cout << "February: 28 days" << endl;
+    break;
+  case 3:
+    cout << "March: 31 days" << endl;
+    break;
+  case 4:
+    cout << "April: 30 days" << endl;
+    break;
+  case 5:
+    cout << "May: 31 days" << endl;
+    break;
+  case 6:
+    cout << "June: 30 days" << endl;
+    break;
+  case 7:
+    cout << "July: 31 days" << endl;
+    break;
+  case 8:
+    cout << "August: 31 days" << endl;
+    break;
+  case 9:
+    cout << "September: 30 days" << endl;
+    break;
+  case 10:
+    cout << "October: 31 days" << endl;
+    break;
+  case 11:
+    cout << "November: 30 days" << endl;
+    break;
+  case 12:
+    cout << "December: 31 days" << endl;
+    break;
+  default:
+    cout << "Такого месяца нет!" << endl;
+    break;
+  }
+} */
