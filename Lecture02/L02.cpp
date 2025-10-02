69a972726cdbc55c96690dd17d13ba28a7068920
Nikita Popov <nikp180407@gmail.com>
2025-10-02 19:04:24 +0200

    L02new

--- Lecture02/L02.cpp
+++ Lecture02/L02.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
 // Number 1
 
 /*int main() {
-  int A, B, C, i;
-  cout << "Число i: ";
-  cin >> i;
-  cout << "Число A: ";
-  cin >> A;
-  cout << "Число B: ";
-  cin >> B;
-  cout << "Число C: ";
-  cin >> C;
-  if (A & (1 << i)) {
+int B, C;
+short A, i;
+cout << "Число i: ";
+cin >> i;
+
+if (i >= 8 || i < 0) {
+  cout << "Слишком большой или малый i" << endl;
+  return 1;
+}
+
+cout << "Число A: ";
+cin >> A;
+bitset<8> b2 = A;
+cout << "Двоичное представление числа A: " << b2.to_string() << endl;
+
+bool iBit = (A >> i) & 1;
+cout << "i бит A: "<< iBit << endl;
+
+  if (iBit) {
+    cout << "Число B: ";
+    cin >> B;
+    cout << "Число C: ";
+    cin >> C;
     if ((A < B) && (B < C)) {
       cout << C-B-A << endl;
     }
@@ -33,46 +47,54 @@ using namespace std;
 // Number 2
 
 /*int main() {
-  int n;
-  cout << "Введите номер месяца: ";
-  cin >> n;
-  switch (n)
+int n;
+cout << "Введите номер месяца: ";
+cin >> n;
+switch (n)
   {
-  case 1:
+  case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+  switch (n) {
+    case 1:
     cout << "January: 31 days" << endl;
     break;
-  case 2:
-    cout << "February: 28 days" << endl;
-    break;
-  case 3:
+    case 3:
     cout << "March: 31 days" << endl;
     break;
-  case 4:
-    cout << "April: 30 days" << endl;
-    break;
-  case 5:
+    case 5:
     cout << "May: 31 days" << endl;
     break;
-  case 6:
-    cout << "June: 30 days" << endl;
-    break;
-  case 7:
+    case 7:
     cout << "July: 31 days" << endl;
     break;
-  case 8:
+    case 8:
     cout << "August: 31 days" << endl;
     break;
-  case 9:
-    cout << "September: 30 days" << endl;
-    break;
-  case 10:
+    case 10:
     cout << "October: 31 days" << endl;
     break;
-  case 11:
-    cout << "November: 30 days" << endl;
-    break;
-  case 12:
+    case 12:
     cout << "December: 31 days" << endl;
+    break;
+  }
+  break;
+  case 4: case 6: case 9: case 11:
+  switch (n) {
+    case 4:
+      cout << "April: 30 days" << endl;
+      break;
+    case 6:
+      cout << "June: 30 days" << endl;
+      break;
+    case 9:
+      cout << "September: 30 days" << endl;
+      break;
+    case 11:
+      cout << "November: 30 days" << endl;
+      break;
+  }
+  break;
+  case 2:
+    cout << "February: 28 days" << endl;
     break;
   default:
     cout << "Такого месяца нет!" << endl;
