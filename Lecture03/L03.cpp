1a9295ddd9b2393c370e5fe841f132a2aa0a7f30
Nikita Popov <nikp180407@gmail.com>
2025-10-05 18:29:31 +0200

    Lecture03

--- Lecture03/L03.cpp
+++ Lecture03/L03.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello wolrd" << std::endl;
+    int x;
+    bool fl = true;
+    std::cin >> x;
+    if (x < 2) {
+        std::cout << "Is not prime" << std::endl;
+    }
+    else {
+        for (int d = 2; d <= sqrt(x); ++d) {
+            if (x % d == 0) {
+                fl = false;
+                break;
+            }
+    }
+    if (fl) {
+        std::cout << "Is prime" << std::endl;
+    }
+    else {
+        std::cout << "Is not prime" << std::endl;
+    }
+    }
     return 0;
 }
