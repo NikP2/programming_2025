34817c0f34b83a5794dd8421915b775338645f24
Nikita Popov <nikp180407@gmail.com>
2025-10-09 20:21:13 +0200

    L03done

--- Lecture03/L03.cpp
+++ Lecture03/L03.cpp
@@ -1,25 +1,71 @@
-#include <iostream>
+//Number 1
+
+/*#include <iostream>
+using namespace std;
+
 
 int main() {
-    int x;
-    bool fl = true;
-    std::cin >> x;
-    if (x < 2) {
-        std::cout << "Is not prime" << std::endl;
+    int n, mx = INT_MIN , su = 0, co = 0, in;
+    cout << "Введите конечное число последовательности: ";
+    cin >> n;
+    if (n <= 0) {
+        cout << "последовательность пуста или отрицательна"<< endl;
+        return 0;
     }
-    else {
-        for (int d = 2; d <= sqrt(x); ++d) {
-            if (x % d == 0) {
-                fl = false;
-                break;
+    bool flMn = false;
+    for (;n != 0;n--) {
+        cout << n <<" Введите число: ";
+        cin >> in;
+        if (in >= 0) {
+            continue;
+        }
+        else {
+            flMn = true;
+            su += in;
+            if (in > mx) {
+                mx = in;
+                co = 1;
             }
+            else if (in == mx) {
+                ++co;
+                }
+            }
+        }
+        if (flMn) {
+            cout << "Наибольше из отрицательных чисел: " << mx << " Сумма чисел: " << su << " Повторы: " << co << endl;
+        }
+        else {
+            cout << "Отрицательных чисел нет" << endl;
+        }
+        return 0;
+    } */
+
+// Number 2
+
+/*#include <iostream>
+using namespace std;
+
+
+int main() {
+    int num, mx = INT_MIN;
+    cout << "Введите число: ";
+    cin >> num;
+    num = abs(num);
+    if (num == 0) {
+        cout << num << endl;
+        return 0; 
     }
-    if (fl) {
-        std::cout << "Is prime" << std::endl;
+    if (num < 1000) {
+        while (num != 0) {
+            if ((num % 10) > mx) {
+                mx = num % 10;
+            }
+            num /= 10;
+        }
+        cout << mx << endl;
     }
     else {
-        std::cout << "Is not prime" << std::endl;
-    }
+       cout << "Не коректное число!" << endl; 
     }
     return 0;
-}
+} */
