237b75108db288dfec9aafdf19d57e0b004865fc
Nikita Popov <nikp180407@gmail.com>
2025-11-21 15:23:46 +0200

    UpdateLO6

--- Lecture06/L06.cpp
+++ Lecture06/L06.cpp
@@ -2,7 +2,84 @@
 
 using namespace std;
 
-void matrixResult(int top, int left, int** mas) {  // передаём нашу матрицу в функцию
+void matrixResult(int top, int left, int** mas) {
+    // Просто выводим матрицу
+    for (int i = 0; i < top; i++) {
+        for (int j = 0; j < left; j++) {
+            cout << mas[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    // создаём 2-х мерный массив с помощью 2-х указателей
+    int** mas = (int**)malloc(2 * sizeof(int*));
+
+    for (int i = 0; i < 2; i++) {
+        mas[i] = (int*)malloc(2 * sizeof(int));
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++){
+            cin >> mas[i][j];
+            if (i == 0 && mas[i][j] < 0) {
+                int fl = 1;
+                while (fl == 1) {
+                    cout << "Введите новое НЕ отрицательное число: ";
+                    cin >> mas[i][j];
+                    if (mas[i][j] >= 0) {
+                        fl = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    int A = mas[0][0];
+    int B = mas[0][1];
+    int C = mas[1][0];
+    int D = mas[1][1];
+
+    int top = 2 + A;
+    int left = 2 + B;
+
+    // расширяю массив с помощью функции realloc
+    mas = (int**)realloc(mas, top * sizeof(int*));
+    
+    for (int i = 2; i < top; i++) {
+        mas[i] = (int*)malloc(left * sizeof(int));
+    }
+
+    // сохраняю данные и заполняем матрицу
+    int old_block[2][2];
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            old_block[i][j] = mas[i][j];
+        }
+    }
+
+    // Расширяем существующие строки 0 и 1
+    for (int i = 0; i < 2; i++) {
+        mas[i] = (int*)realloc(mas[i], left * sizeof(int));
+    }
+
+    // Заполняем ВСЮ матрицу формулой
+    for (int i = 0; i < top; i++) {
+        for (int j = 0; j < left; j++) {
+            mas[i][j] = i * C + j * D;
+        }
+    }
+
+    // Восстанавливаем исходный блок в правом нижнем углу
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            mas[i + A][j + B] = old_block[i][j];
+        }
+    }
+
+    matrixResult(top, left, mas);
+
     int coZ = 0;
     int* zeroRow = 0; 
     // Есть ли нули в строке или нет (если есть хоть 1 нолик, то переходим на следующую строку)
@@ -35,143 +112,39 @@ void matrixResult(int top, int left, int** mas) {  // передаём нашу 
         }
         cout << endl;
 
-        // создаём новый масив, где колличество элементов будет взято от предыдущего - кол-во строк с нулями (данный шаг необходим для последующего удаления строк, которые содержат нули)
-        int newTop = top - coZ;
-        int** newMas = (int**)malloc(newTop * sizeof(int*));
+    // Удаляем строки с нулями из исходного массива
+    int newTop = top;
+    for (int i = 0; i < newTop; i++) {
+        bool flZ = false;
         
-        int newIndex = 0;
-        for (int i = 0; i < top; i++) {
-            int rem = 0; // создаём флаг
-            
-            for (int k = 0; k < coZ; k++) {
-                if (i == zeroRow[k]) { // если строка будет равна строке, где мы нашли ноль, то...
-                    rem = 1; // если флаг принял значение true / 1, то в дальнейшем мы удалим эту строку
-                    break;
-                }
-            }
-            
-            // если строка не удаляется, значит мы её скопируем в новый масив.
-            if (rem == 0) {
-                newMas[newIndex] = (int*)malloc(left * sizeof(int));
-                for (int j = 0; j < left; j++) {
-                    newMas[newIndex][j] = mas[i][j];
-                }
-                newIndex++;
+        // Проверяем, есть ли в строке нули
+        for (int j = 0; j < left; j++) {
+            if (mas[i][j] == 0) {
+                flZ = true;
+                break;
             }
         }
         
-        for (int i = 0; i < newTop; i++) {
-            for (int j = 0; j < left; j++) {
-                cout << newMas[i][j] << " ";
-            }
-            cout << endl;
-        }
-        
-        for (int i = 0; i < top; i++) {
+        // Если нашли строку с нулем, удаляем её
+        if (flZ) {
             free(mas[i]);
-        }
-        free(mas);
-        free(zeroRow);
-    } 
-    else {
-        cout << "Строки с нулями не найдены..." << endl;
-        for (int i = 0; i < top; i++) {
-            for (int j = 0; j < left; j++) {
-                cout << mas[i][j] << " ";
+            
+            // Сдвигаем все последующие строки на одну позицию вверх
+            for (int k = i; k < newTop - 1; k++) {
+                mas[k] = mas[k + 1];
             }
-            cout << endl;
-        }
-    }
-}
-
-int main() {
-
-    // создаём 2-х мерный массив с помощью 2-х указателей. Один указатель будет содержать адреса строк, другой указатель будет содержать элементы этой строки. 
-    int** mas = (int**)malloc(2 * sizeof(int*));
-
-    for (int i = 0; i < 2; i++) {
-        mas[i] = (int*)malloc(2 * sizeof(int));
-    }
-
-    // проходимся по массиву и проверяем, чтобы A и B не были отрицательными. Тут я тоже использовал флаг.
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++){
-            cin >> mas[i][j];
-            if (i == 0 && mas[i][j] < 0) {
-                int fl = 1;
-                while (fl == 1) {
-                    cout << "Введите новое НЕ отрицательное число: ";
-                    cin >> mas[i][j];
-                    if (mas[i][j] >= 0) {
-                        fl = 0;
-                    }
-                }
-            }
+            newTop--;
+            i--;
         }
     }
 
-    int A = mas[0][0];
-    int B = mas[0][1];
-    int C = mas[1][0];
-    int D = mas[1][1];
-
-    int top = 2 + A;
-    int left = 2 + B;
-
-    // расширяю массив с помощью функции realloc. Всё на подобии, как было в начале main
-    mas = (int**)realloc(mas, top * sizeof(int*));
-    
-    for (int i = 2; i < top; i++) {
-        mas[i] = (int*)malloc(left * sizeof(int));
-    }
-    
-
-    // чтобы не потерять значение при расширении, мы сохраняем наши числа: A B C D
-    for (int i = 0; i < 2; i++) {
-        int old_data[2];
-        for (int j = 0; j < 2; j++) {
-            old_data[j] = mas[i][j];
-        }
-        
-        mas[i] = (int*)realloc(mas[i], left * sizeof(int)); // расширение строки
-        
-        for (int j = 0; j < left; j++) {
-            mas[i][j] = i * C + j * D; // заполняем всю строку формулой данной в условии
-        }
-        
-        for (int j = 0; j < 2; j++) {
-            mas[i][j + B] = old_data[j];  // воастанавливаем старые элементы справа
-        }
-    }
-    
-    for (int i = 2; i < top; i++) {
-        for (int j = 0; j < left; j++) {
-            mas[i][j] = i * C + j * D;   // заполняем строки, которые мы добавили сверху
-        }
+    // Изменяем размер массива указателей
+    if (newTop < top) {
+        mas = (int**)realloc(mas, newTop * sizeof(int*));
+        top = newTop;
     }
 
-    int** ugol = (int**)malloc(2 * sizeof(int*));
-    for (int i = 0; i < 2; i++) {
-        ugol[i] = (int*)malloc(2 * sizeof(int));
-        for (int j = 0; j < 2; j++) {
-            ugol[i][j] = mas[i][j + B];  // сохраняем данные, чтобы переместить их в нужный угол
-        }
-    }
-    
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            mas[i + A][j + B] = ugol[i][j];
-        }
-    }
-    // если ячейка левее или выше блока, то заполняем нашей формцлой
-    for (int i = 0; i < top; i++) {
-        for (int j = 0; j < left; j++) {
-            if (i < A || j < B ) {
-                mas[i][j] = i * C + j * D;
-            }
-        }
-    }
-
+    // Выводим результат после удаления строк с нулями
     for (int i = 0; i < top; i++) {
         for (int j = 0; j < left; j++) {
             cout << mas[i][j] << " ";
@@ -179,17 +152,15 @@ int main() {
         cout << endl;
     }
 
-    matrixResult(top, left, mas);
-
-    for (int i = 0; i < 2; i++) {
-        free(ugol[i]);
+    // Освобождаем память
+    for (int i = 0; i < top; i++) {
+        free(mas[i]);
     }
-    free(ugol);
+    free(mas);
     
     return 0;
 }
-
-
+}
 // Задание №2
 
 // #include <iostream>
